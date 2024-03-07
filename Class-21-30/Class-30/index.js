1dd111ceed2808aa9f19964a7dd2637c9d819a93
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-07 22:48:11 +0600

    do while loop

---
+++ Class-21-30/Class-30/index.js
@@ -0,0 +1,15 @@
+// Class :: 29 do while loop
+
+// Ex:01
+let i = 1
+let isRunning = true
+do {
+    console.log("Hi")
+    i++
+} while (isRunning && i <= 3)
+
+// Ex:02
+do {
+    console.log("Hello")
+    isRunning = false
+} while (isRunning)
