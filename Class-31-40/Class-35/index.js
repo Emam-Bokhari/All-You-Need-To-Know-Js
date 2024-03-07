fe5e7c2e9a7ffafd8fa5e1e07475c8ee2eb866ed
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-07 23:11:47 +0600

    infinity for loop

---
+++ Class-31-40/Class-35/index.js
@@ -0,0 +1,14 @@
+// Class :: 35 infinity for loop
+
+// Ex:01 
+for (; ;) {
+    const randomNumber=Math.round(Math.random()*10)
+    if(randomNumber===5){
+        console.log(randomNumber)
+        break;
+    }else{
+        console.log(randomNumber)
+    }
+        
+}
+
