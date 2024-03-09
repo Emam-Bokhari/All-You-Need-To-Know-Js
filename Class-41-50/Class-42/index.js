bf7968f973d2c7db387f0694a38dcf24338ba4ee
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 20:30:30 +0600

    array declaration

---
+++ Class-41-50/Class-42/index.js
@@ -0,0 +1,22 @@
+// Class :: 42 Array declaration
+
+// Ex:01
+const arr = [1, 2, 3, 4, 5]
+console.log(arr[0])
+
+// Ex:02
+let num = [1, 2, 3]
+num[0] = 29
+console.log(num)
+
+// Ex:03
+const num1 = [1, 2, 3, 4]
+console.log(num1.length)
+
+// Ex:04
+const num2 = Array()
+console.log(num2)
+
+// Ex:05
+const num3 = Array(0, 1, 2, 3, 4, 5)
+console.log(num3)
