006ffc5a6bc7e1148676b020034d5fbce07cb9a3
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 19:19:21 +0600

    string comparison

---
+++ Class-31-40/Class-39/index.js
@@ -0,0 +1,26 @@
+// Class :: 39 string comparison
+
+// Ex:01
+const x="b"
+const y="a"
+console.log(x>y)
+
+// Ex:02
+const a="A"
+const b="Z"
+console.log(b>a)
+
+// Ex:03
+const c="A"
+const d="z"
+console.log(c>d)
+
+// Ex:04
+const e="aa"
+const f="az"
+console.log(f>e)
+
+// Ex:05
+const g="20"
+const h=21
+console.log(g>h)
