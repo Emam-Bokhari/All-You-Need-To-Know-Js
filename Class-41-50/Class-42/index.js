051d04f5d2bbe27225b2c82d443ad0eb5b028bf7
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 20:43:59 +0600

    Array traversing

--- Class-41-50/Class-42/index.js
+++ Class-41-50/Class-42/index.js
@@ -4,19 +4,19 @@
 const arr = [1, 2, 3, 4, 5]
 console.log(arr[0])
 
-// Ex:02
+// Ex:02 include new data in array
 let num = [1, 2, 3]
 num[0] = 29
 console.log(num)
 
-// Ex:03
+// Ex:03 Array length
 const num1 = [1, 2, 3, 4]
 console.log(num1.length)
 
-// Ex:04
+// Ex:04 Array constractor
 const num2 = Array()
 console.log(num2)
 
-// Ex:05
+// Ex:05 Array constractor
 const num3 = Array(0, 1, 2, 3, 4, 5)
 console.log(num3)
---
+++ Class-41-50/Class-43/index.js
@@ -0,0 +1,23 @@
+// Class :: 44 Array traversing
+
+// Ex:01
+let arr = [0, 1, 2, 3, 4]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+// Ex:02
+let num = [0, 1, 2, 3, 4, 5]
+for (let i = 0; i < num.length; i++) {
+    num[i]+=1
+
+}
+console.log(num)
+
+// Ex:03
+let num1=[0,1,2,3,4,5,6]
+let sum=0
+for(let i=0;i<num1.length;i++){
+    sum+=num1[i]
+}
+console.log(sum)
