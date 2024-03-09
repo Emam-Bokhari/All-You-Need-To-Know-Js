622ab0e9776e598ecd2f82b698d90c9af2a2c86f
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 20:16:35 +0600

    string length

---
+++ Class-41-50/Class-41/index.js
@@ -0,0 +1,12 @@
+// Class :: 41 string length
+
+const x="Emam Bokhari"
+let length=0
+while(true){
+    if(x.charAt(length)===""){
+        break  
+    }else{
+        length++
+    }
+}
+console.log(length)
