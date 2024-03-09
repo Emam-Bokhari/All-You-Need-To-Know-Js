614dad929695f4fdec517f0d524ada61bd96ce77
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 19:31:57 +0600

    string methods

---
+++ Class-31-40/Class-40/index.js
@@ -0,0 +1,50 @@
+// Class :: 40 string methods
+
+/* 
+1 concat
+2 startsWith
+3 endsWith
+4 split
+5 substring
+6 chartAt
+7 toUpperCase
+8 toLowerCase
+9 trim
+*/
+
+// Ex: concat
+const x="Emam"
+const y="Bokhari"
+console.log(x.concat(" ").concat(y))
+
+// Ex: startsWith
+const a="This is apple"
+console.log(a.startsWith("This"))
+
+// Ex: endsWith
+const b="This is banana"
+console.log(b.endsWith("mango"))
+
+// Ex: split
+const c="This is Moshfiqur Rahman Emam Bokhari"
+console.log(c.split(" "))
+
+// Ex: substring
+const d="Hello World!"
+console.log(d.substring(0,5))
+
+// Ex: chartAt
+const e="Emam Bokhari"
+console.log(e.charAt(2))
+
+// Ex: toUpperCase
+const f="emam bokhari"
+console.log(f.toUpperCase())
+
+// Ex: toLowerCase
+const g="Emam Bokhari"
+console.log(g.toLowerCase())
+
+// Ex: trim
+const h="            Emam Bokhari       "
+console.log(h.trim())
