ad4f4fc4000afa9b06f227291f69f800c169a775
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-09 19:09:04 +0600

    String

---
+++ Class-31-40/Class-37/index.js
@@ -0,0 +1,17 @@
+// Class :: 37  String leteral
+
+// Ex:01 
+const str="Emam Bokhari"
+console.log(str)
+
+// Ex:02
+const str2=25
+console.log(String(str2))
+
+// Ex:03
+const n=25
+console.log(n+"")
+
+// Ex:04
+const n2=25
+console.log(n2.toString())
