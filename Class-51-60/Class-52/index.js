be90c107915c372be688680ef4f5abc5afd556c0
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-23 19:30:20 +0600

    accessing object properties

---
+++ Class-51-60/Class-52/index.js
@@ -0,0 +1,37 @@
+// Class :: 52 Accessing object properties
+
+/* 
+* dot notation
+* array notaion
+*/
+
+// Ex: dot notation
+const dot={
+    physics:20,
+    chemistry:30,
+    biology:40 
+}
+
+console.log(dot.physics)
+
+// Ex: array notation
+const array={
+    physics:30,
+    biology:40,
+    math:50 
+}
+
+console.log(array["physics"])
+
+// addition 2 object properties 
+const obj={
+    math:30,
+    biology:40 
+}
+
+const obj2={
+    highermath:60,
+    ict:100 
+}
+
+console.log(obj.math+obj2.highermath)
