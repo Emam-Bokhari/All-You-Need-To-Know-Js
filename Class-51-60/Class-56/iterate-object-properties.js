2c293ce52d3b990234c107184341222e6fed22e9
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-23 19:50:43 +0600

    iterate object properties

---
+++ Class-51-60/Class-56/iterate-object-properties.js
@@ -0,0 +1,13 @@
+// Class :: 55 Iterate object properties
+
+const marks={
+    math:55,
+    biology:100,
+    ict:60,
+    physics:67 
+}
+
+for(let i in marks){
+    console.log(i)
+    console.log(marks[i])
+}
