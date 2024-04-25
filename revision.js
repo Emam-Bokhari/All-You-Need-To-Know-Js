9f7f7e5dadb23a32c4c93d65651c008aacfdd619
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-04-25 22:41:34 +0600

    condition

--- revision.js
+++ revision.js
@@ -33,3 +33,54 @@ without falsy values , all truthy value
 Constractor:
 String,Number,Bollean
 */
+
+// :: Class-15
+/* 
+Atithmetic operator
+1. Addition
+2. substraction
+3. Multiplication
+4. Division
+5. Modolus
+6. Incremental
+7. Decremental
+
+pre increment
+post increment
+
+pre decremental
+post decremental
+*/
+
+// :: Class-21
+/* 
+1. Even Number
+2. Odd Number
+3. Large Number
+*/
+// Even Number
+const x=20
+const y=21
+if(x%2===0){
+    console.log("Even Number",x)
+}else{
+    console.log("Odd Number")
+}
+
+// Odd Number
+if(y%2===1){
+    console.log("Odd Number",y)
+}else{
+    console.log("Even Number")
+}
+
+// Large Number
+if(x<y){
+    console.log("Y is greater than x")
+}else if(x>y){
+    console.log("X is greater than y")
+}else if(x===y){
+    console.log("X and Y are same value")
+}else{
+    console.log("Please enter valid number")
+}
