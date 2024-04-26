7393f06a8df8724149476ea977a101b01d151a35
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-04-26 11:25:14 +0600

    array constructor

--- Revision/class-41-56.js
+++ Revision/class-41-56.js
@@ -1,4 +1,5 @@
 // :: Class-43
+// array declaration
 const a=[1,2,3,4,5]
 a[5]=46
 console.log(a)
@@ -6,6 +7,10 @@ console.log(a)
 const b=[2,20,34,5]
 b[0]=10
 console.log(b.length,b)
+
+// array constructor
+const c=Array(1,2,3,4)
+console.log(c)
 
 
 
