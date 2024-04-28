fb8ebb52463d07b053a6475abc569ff1d7508f58
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-04-28 11:35:44 +0600

    keys,values,entries,traversing,clone

--- Class-51-60/Class-57/object-method.js
+++ Class-51-60/Class-57/object-method.js
@@ -1,2 +1,26 @@
 // Class :: 55 object method in js
+// values,keys,entries,clone object
 
+// keys
+const x={
+    math:20,
+    biology:10,
+    chemistry:21 
+}
+console.log(Object.keys(x))
+
+// values
+console.log(Object.values(x))
+
+// entries
+const ent=(Object.entries(x))
+console.log(ent)
+
+// traversing
+for(let i=0;i<ent.length;i++){
+    console.log(ent[i])
+}
+
+// clone
+let obj=Object.assign({},x)
+console.log(obj)
